51784f0cc425e63f53de52ba4671170c8669cf0f
OHINATA-HPPC\darix <darixianga@gmail.com>
2025-12-05 00:46:42 +0900

    feature: ダークテーマを削除

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,5 @@
 import { useColorScheme } from 'react-native';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
