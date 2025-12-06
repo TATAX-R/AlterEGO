957b1129470777e42f27c6200004baec7756ed90
asahhiy <asahhiy@gmail.com>
2025-12-07 06:24:40 +0900

    clean:最初の起動ボタンを関数BootCameraButton.tsxに切り分け

--- app/(tabs)/camerademo.tsx
+++ app/(tabs)/camerademo.tsx
@@ -1,21 +1,7 @@
-import { Button, Text, XStack, YStack } from 'tamagui';
-import { useRouter } from 'expo-router';
-import { useFoodScan } from '@/hooks/useFoodScan';
+import BootCameraButton from '@/components/BootCameraButton';
+import { YStack, Text } from 'tamagui';
 
 export default function App() {
-  const router = useRouter(); //ナビゲーション用
-  const { startCamera } = useFoodScan();
-
-  const handlePress = async () => {
-    const img = await startCamera();
-    if (img && img !== 'error') {
-      //imgが存在するならrouterでpush
-      router.push({
-        pathname: '/recordfood',
-        params: { imageUriFirst: img },
-      });
-    }
-  };
   return (
     <YStack
       backgroundColor="$color1"
@@ -32,9 +18,7 @@ export default function App() {
         fontWeight={800}>
         ホーム画面のつもり
       </Text>
-      <Button animation="bouncy" w={150} pressStyle={{ scale: 0.9 }} onPress={handlePress}>
-        写真を撮る
-      </Button>
+      <BootCameraButton />
     </YStack>
   );
 }
