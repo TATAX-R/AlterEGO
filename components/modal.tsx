270b8ec89fc641fb56a6118b700238ee2a932011
Ayu102938 <darixianga@gmail.com>
2025-12-07 00:24:20 +0900

    feature: add modal components (#19)
    * feature: モーダルコンポーネントの実装とapp/_layoutを少し変更
    
    * feature: ボタンのコンポーネントをモーダルから分離
    
    * feature: モーダルのトリガーを外部から起こすように変更
    
    * fix: Symptom全体ではなく、必要な2つ('tipsTitle' | 'tipsContent')だけを Pick
    
    * fix: useColorSchemeを削除
    
    * feature: スプラッシュスクリーンを隠す処理にエラーハンドリングを追加
    
    * fix: スプラッシュスクリーンの削除
    
    * fix: add/_layout.tsxの変更を破棄
    
    * fix: _layput.tsxの状態を初期に戻しました
    
    * fix: ModalPropsのpickで分解されてた箇所の修正

---
+++ components/modal.tsx
@@ -0,0 +1,72 @@
+import { Button, Dialog, YStack, XStack, Separator } from 'tamagui';
+import { Symptom } from '@/types';
+
+type ModalProps = {
+  tipsTitle: string;
+  tipsContent: string;
+
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+};
+
+export function Modal({ open, onOpenChange, tipsTitle, tipsContent }: ModalProps) {
+  return (
+    <Dialog modal open={open} onOpenChange={onOpenChange}>
+      <Dialog.Portal>
+        <Dialog.Overlay
+          key="overlay"
+          animation={[
+            'quick',
+            {
+              opacity: {
+                overshootClamping: true,
+              },
+            },
+          ]}
+          opacity={0.5}
+          enterStyle={{ opacity: 0 }}
+          exitStyle={{ opacity: 0 }}
+          backgroundColor="black"
+        />
+
+        <Dialog.Content
+          bordered
+          elevate
+          key="content"
+          animateOnly={['transform', 'opacity']}
+          animation={[
+            'quick',
+            {
+              opacity: {
+                overshootClamping: true,
+              },
+            },
+          ]}
+          enterStyle={{ x: 0, y: -20, opacity: 0, scale: 0.9 }}
+          exitStyle={{ x: 0, y: 10, opacity: 0, scale: 0.95 }}
+          gap="$4"
+          width="90%"
+          maxWidth={400}
+          backgroundColor="$background">
+          <YStack gap="$2">
+            <Dialog.Title size="$9" fontWeight="bold" color="$color">
+              {tipsTitle}
+            </Dialog.Title>
+
+            <Separator borderColor="$color" />
+
+            <Dialog.Description size="$4" fontWeight="bold" color="$color" lineHeight={22}>
+              {tipsContent}
+            </Dialog.Description>
+          </YStack>
+
+          <XStack theme="accent" gap="$3" justifyContent="center">
+            <Dialog.Close asChild>
+              <Button>閉じる</Button>
+            </Dialog.Close>
+          </XStack>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog>
+  );
+}
