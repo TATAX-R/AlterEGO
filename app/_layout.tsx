62ce82c8065107b0cfdf98ce553a03e635913c77
OHINATA-HPPC\darix <darixianga@gmail.com>
2025-12-06 20:35:38 +0900

    fix: useColorSchemeを削除

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,3 @@
-import { useColorScheme } from 'react-native';
 import { DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
@@ -9,18 +8,14 @@ import { TamaguiProvider } from 'tamagui';
 
 import config from '../tamagui.config';
 
-// 起動画面（ロゴ）を自動で消さないように指示
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  // const colorScheme = useColorScheme();
-
   const [loaded] = useFonts({
     Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
     InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
   });
 
-  // フォント読み込み完了後に起動画面を隠す
   useEffect(() => {
     if (loaded) {
       SplashScreen.hideAsync();
