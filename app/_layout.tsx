0dbb1a0151bd23658ccbda09890785467ba4d57e
OHINATA-HPPC\darix <darixianga@gmail.com>
2025-12-06 22:09:01 +0900

    fix: スプラッシュスクリーンの削除

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,26 +1,17 @@
 import { DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { TamaguiProvider } from 'tamagui';
 
 import config from '../tamagui.config';
 
-SplashScreen.preventAutoHideAsync();
-
 export default function RootLayout() {
   const [loaded] = useFonts({
     Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
     InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
   });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync().catch(console.error);
-    }
-  }, [loaded]);
 
   if (!loaded) {
     return null;
