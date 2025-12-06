485071ce1bb3593c69d9dc6f809c9cfb83a79463
OHINATA-HPPC\darix <darixianga@gmail.com>
2025-12-07 01:47:30 +0900

    feature: タブバーでヘルスページ(健康状態)のタブバーアイコンの設定

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -30,6 +30,15 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="health"
+        options={{
+          title: 'Health',
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="waveform.path.ecg" color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
