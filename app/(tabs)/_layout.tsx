0c61131de839c5f3136495a24adbc495ea671a15
OHINATA-HPPC\darix <darixianga@gmail.com>
2025-12-07 10:36:43 +0900

    feature: タブバーアイコンをtamagui製に変えたのと、色も変えました

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,14 +1,11 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 import { useTheme } from 'tamagui';
+import { Home, Footprints, ActivitySquare } from '@tamagui/lucide-icons';
 
 import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
 
 export default function TabLayout() {
-  // Tamaguiのテーマを取得
-  // app/_layout.tsx で defaultTheme="light" に固定されていれば、
-  // ここでは自動的にライトテーマの色が取得されます。
   const theme = useTheme();
 
   return (
@@ -16,11 +13,9 @@ export default function TabLayout() {
       screenOptions={{
         tabBarStyle: {
           backgroundColor: theme.color2.val,
-
           borderTopColor: theme.borderColor?.val ?? theme.color2.val,
         },
         tabBarActiveTintColor: theme.color.val,
-
         headerShown: false,
         tabBarButton: HapticTab,
       }}>
@@ -28,7 +23,7 @@ export default function TabLayout() {
         name="steps/index"
         options={{
           title: 'Steps',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="figure.walk" color={color} />,
+          tabBarIcon: ({ color }) => <Footprints size={28} color={color} />,
         }}
       />
 
@@ -36,7 +31,7 @@ export default function TabLayout() {
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({ color }) => <Home size={28} color={color} />,
         }}
       />
 
@@ -44,9 +39,7 @@ export default function TabLayout() {
         name="health"
         options={{
           title: 'Health',
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="waveform.path.ecg" color={color} />
-          ),
+          tabBarIcon: ({ color }) => <ActivitySquare size={28} color={color} />,
         }}
       />
     </Tabs>
